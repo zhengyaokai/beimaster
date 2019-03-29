98ec732eb1741faf1d2f5f76d0190e6ca4f7c54a
root <378856081@qq.com>
2019-03-29 13:56:54 +0800

    springboot项目初始提交

---
+++ src/main/java/com/bmatser/BmatserApplication.java
@@ -0,0 +1,13 @@
+package com.bmatser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmatserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BmatserApplication.class, args);
+	}
+
+}
